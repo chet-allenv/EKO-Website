03a154bea2b4ab1e196527d765d4ca177d3737be
Chet Allen <chet.allenv@gmail.com>
2025-04-17 22:33:20 -0400

    Commit

---
+++ gallery.js
@@ -0,0 +1,63 @@
+class GalleryImage {
+    constructor(src, alt, caption) {
+        this.src = src;
+        this.alt = alt;
+        this.caption = caption;
+    }
+
+    getSrc() {
+        return this.src;
+    }
+
+    getAlt() {
+        return this.alt;
+    }
+
+    getCaption() {
+        return this.caption;
+    }
+
+    setSrc(src) {
+        this.src = src;
+    }
+
+    setAlt(alt) {
+        this.alt = alt;
+    }
+
+    setCaption(caption) {
+        this.caption = caption;
+    }
+}
+
+const image1 = new GalleryImage("res/interesting_meeting_1_poster.png", "First Interest Meeting Poster", "Interest Meeting 1 Poster!");
+const image2 = new GalleryImage("res/interesting_meeting_2_poster.png", "Second Interest Meeting Poster", "Interest Meeting 2 Poster!");
+const image3 = new GalleryImage("res/eko_table.jpg", "EKO Table", "EKO Table at the Student Org Showcase, featuring VP Chet and Treasurer Nick!");
+const image4 = new GalleryImage("res/chet_presentation.jpg", "Chet Presentation", "VP Chet presenting at the Second Interest Meeting!");
+const image5 = new GalleryImage("res/tyropita.jpg", "Tyropita", "Tyropita made for our first interest meeting!");
+
+let images = [image1, image2, image3, image4, image5];
+
+const leftButton = document.getElementById("button-left");
+const rightButton = document.getElementById("button-right");
+
+const image = document.getElementById("gallery-image");
+const imageCaption = document.getElementById("gallery-caption");
+
+updateImage();
+
+leftButton.addEventListener("click", () => {
+    images.unshift(images.pop());
+    updateImage();
+});
+
+rightButton.addEventListener("click", () => {
+    images.push(images.shift());
+    updateImage();
+});
+
+function updateImage() {
+    image.src = images[0].getSrc();
+    image.alt = images[0].getAlt();
+    imageCaption.innerText = images[0].getCaption();
+}
